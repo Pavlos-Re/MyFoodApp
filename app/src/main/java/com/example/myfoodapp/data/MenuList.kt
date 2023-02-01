438cde52d0cc16daff8e2bdc017cc06795e88d0a
Pavlos-Re <pavlos.repin@gmail.com>
2023-02-01 02:17:23 +0200

    Project is done.

--- app/src/main/java/com/example/myfoodapp/data/MenuList.kt
+++ app/src/main/java/com/example/myfoodapp/data/MenuList.kt
@@ -73,10 +73,10 @@ object MenuList{
 
         if (id == "id06") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
+                Menu("Chicken Burger", 4.5),
+                Menu("Beef Burger", 5.0),
+                Menu("Bacon Cheese Burger", 7.0),
+                Menu("Veggie Burger", 4.0),
                 Menu("Fry Potatoes", 2.0),
                 Menu("Soda Drink", 1.0)
             )
@@ -84,43 +84,43 @@ object MenuList{
 
         if (id == "id07") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
-                Menu("Fry Potatoes", 2.0),
+                Menu("Cheddar Bacon Topping", 3.5),
+                Menu("Sweet Chilli Sauce", 3.0),
+                Menu("Barbecue Sauce", 3.0),
+                Menu("Sour Cream Sauce", 3.0),
+                Menu("Classic Style", 2.0),
                 Menu("Soda Drink", 1.0)
             )
         }
 
         if (id == "id08") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
-                Menu("Fry Potatoes", 2.0),
-                Menu("Soda Drink", 1.0)
+                Menu("Vanilla Flavour", 1.5),
+                Menu("Chocolate Flavour", 1.5),
+                Menu("Strawberry Flavour", 1.5),
+                Menu("Triple Classic Flavour", 2.0),
+                Menu("Fruit Flavour", 1.5),
+                Menu("Popsicle", 1.0)
             )
         }
 
         if (id == "id09") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
-                Menu("Fry Potatoes", 2.0),
+                Menu("Pepperoni Pizza", 7.5),
+                Menu("Classic Pizza", 6.0),
+                Menu("Greek Pizza", 8.0),
+                Menu("Mozzarella Pizza", 7.5),
+                Menu("Chicken Pizza", 8.0),
                 Menu("Soda Drink", 1.0)
             )
         }
 
         if (id == "id10") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
+                Menu("Veal Steak", 6.5),
+                Menu("Chicken Steak", 6.0),
+                Menu("Beef Steak", 6.0),
+                Menu("Pork Chops", 7.0),
                 Menu("Fry Potatoes", 2.0),
                 Menu("Soda Drink", 1.0)
             )
@@ -128,54 +128,54 @@ object MenuList{
 
         if (id == "id11") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
-                Menu("Fry Potatoes", 2.0),
-                Menu("Soda Drink", 1.0)
+                Menu("Chicken Noodles", 4.5),
+                Menu("Pork Noodles", 3.0),
+                Menu("Beef Noodles", 3.0),
+                Menu("Veggies Noodles", 4.0),
+                Menu("Shrimp Noodles", 2.0),
+                Menu("Tea", 1.0)
             )
         }
 
         if (id == "id12") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
-                Menu("Fry Potatoes", 2.0),
-                Menu("Soda Drink", 1.0)
+                Menu("Sushi", 3.5),
+                Menu("Bao Buns", 3.0),
+                Menu("Noodles", 3.0),
+                Menu("Sashimi", 4.0),
+                Menu("Veggie Spring Rolls", 2.0),
+                Menu("Tea", 1.0)
             )
         }
 
         if (id == "id13") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
-                Menu("Fry Potatoes", 2.0),
+                Menu("Chicken Sandwich", 4.5),
+                Menu("Egg Sandwich", 3.0),
+                Menu("Beef Sandwich", 4.0),
+                Menu("Mixed Sandwich", 5.0),
+                Menu("Veggies Sandwich", 2.0),
                 Menu("Soda Drink", 1.0)
             )
         }
 
         if (id == "id14") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
-                Menu("Fry Potatoes", 2.0),
-                Menu("Soda Drink", 1.0)
+                Menu("Vanilla Pie", 4.5),
+                Menu("Chocolate Pie", 3.0),
+                Menu("Cream Pie", 3.0),
+                Menu("Jam Pie", 4.0),
+                Menu("Vegan Pie", 2.0),
+                Menu("Chocolate Milk", 1.0)
             )
         }
 
         if (id == "id15") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
+                Menu("Classic Hot Dog", 1.5),
+                Menu("Chicken Hot Dog", 2.0),
+                Menu("Special Hot Dog", 3.0),
+                Menu("Vegan Hot Dog", 4.0),
                 Menu("Fry Potatoes", 2.0),
                 Menu("Soda Drink", 1.0)
             )
@@ -183,21 +183,21 @@ object MenuList{
 
         if (id == "id16") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
-                Menu("Fry Potatoes", 2.0),
-                Menu("Soda Drink", 1.0)
+                Menu("Bread Loaf", 0.5),
+                Menu("Cookies", 3.0),
+                Menu("Cake", 7.0),
+                Menu("Chocolate Cake", 8.0),
+                Menu("Butter Milk Cookies", 3.0),
+                Menu("Milk Cake", 8.0)
             )
         }
 
         if (id == "id17") {
             menuList = listOf(
-                Menu("Green Salad", 4.5),
-                Menu("Chicken Kebab", 3.0),
-                Menu("Beef Kebab", 3.0),
-                Menu("Mixed Kebab", 4.0),
+                Menu("Fish and chips", 4.5),
+                Menu("Fish Sticks", 3.0),
+                Menu("Fried Fish", 3.0),
+                Menu("Baked Fish", 4.0),
                 Menu("Fry Potatoes", 2.0),
                 Menu("Soda Drink", 1.0)
             )
